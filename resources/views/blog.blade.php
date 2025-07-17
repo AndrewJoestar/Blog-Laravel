4943f8ee9ba71f512834c79459b164827d7e7f8b
Andrew Ikhtisar Afiq <andrew2000018073@webmail.uad.ac.id>
2025-07-17 17:02:28 +0700

    Initial commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman blog</title>
+</head>
+
+<body>
+    <h1>
+        TBA Halaman Blog
+    </h1>
+
+    <article>
+        <h3>
+            Judul Tulisan
+
+        </h3>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium blanditiis cupiditate officiis
+            reprehenderit, ullam nisi soluta quibusdam eius molestiae dolores eos, tenetur magnam laboriosam tempora
+            doloremque, illo sit mollitia. Provident?
+        </p>
+    </article>
+    <article>
+        <h3>
+            Judul Tulisan 2
+
+        </h3>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium blanditiis cupiditate officiis
+            reprehenderit, ullam nisi soluta quibusdam eius molestiae dolores eos, tenetur magnam laboriosam tempora
+            doloremque, illo sit mollitia. Provident?
+        </p>
+    </article>
+</body>
+
+</html>
